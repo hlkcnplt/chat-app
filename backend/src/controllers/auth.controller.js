351a58e7595226640ed761a19a8e1a81eba7ecab
Haluk <halukcnplt@gmail.com>
2025-07-06 15:10:26 +0300

    refactor: formatted code

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,11 +1,9 @@
-export const signup = (req, res) => {
-    
-};
+export const signup = (req, res) => {};
 
 export const login = (req, res) => {
-    res.send("login route")
+  res.send("login route");
 };
 
 export const logout = (req, res) => {
-    res.send("logout route")
+  res.send("logout route");
 };
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,6 @@
 import express from "express";
-import dotenv from "dotenv"
-import authRoutes from "./routes/auth.route.js"
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
@@ -11,6 +11,6 @@ const PORT = process.env.PORT;
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-    console.log('soerver is running on PORT:' + PORT)
-    connectDB();
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
 });
